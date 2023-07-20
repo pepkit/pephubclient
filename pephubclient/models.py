18a8c53af8b0bbcc4000cf38b4171818786eca11
Khoroshevskyi <sasha99250@gmail.com>
2023-07-20 11:19:56 -0400

    Updated const in models

--- pephubclient/models.py
+++ pephubclient/models.py
@@ -2,6 +2,7 @@ from typing import Optional
 
 import pydantic
 from pydantic import BaseModel, Extra, Field
+from peppy.const import CONFIG_KEY, SUBSAMPLE_RAW_LIST_KEY, SAMPLE_RAW_DICT_KEY
 
 
 class ProjectDict(BaseModel):
@@ -9,9 +10,9 @@ class ProjectDict(BaseModel):
     Project dict (raw) model
     """
 
-    config: dict = Field(alias="_config")
-    subsample_list: Optional[list] = Field(alias="_subsample_list")
-    sample_list: list = Field(alias="_sample_dict")
+    config: dict = Field(alias=CONFIG_KEY)
+    subsample_list: Optional[list] = Field(alias=SUBSAMPLE_RAW_LIST_KEY)
+    sample_list: list = Field(alias=SAMPLE_RAW_DICT_KEY)
 
     class Config:
         allow_population_by_field_name = True
